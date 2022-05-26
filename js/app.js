30d6d1508b8120ef6f61c4469b0123c653591d91
acescon <acescon@visa.it>
2022-05-26 16:39:04 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-if(document.getElementById('app')) {
-	document.getElementById('app').innerHTML="ciao 1";
-}
+document.addEventListener('load', function() {
+    alert("Ready!");
+	var ticket= new Ticket('26-05-2022','alessandro cescon','123456','25-05-2022','001');
+	var eqi = new Equipment('GEN_SET','999999');
+	ticket.addequi(eqi);
+	var wor = new Work();
+	ticket.addwork(wor);
+	var mat = new Material();
+	ticket.addmaterial(mat);
+	var not = new Note();
+	ticket.addnote(not);
+	var tsi = new Signature();
+	ticket.addtecsign(tsi);
+	var csi = new Signature();
+	ticket.addclisign(csi);
+	console.log(ticket);
+}, false);
--- js/classes.js
+++ js/classes.js
@@ -6,7 +6,7 @@ class Ticket {
     this._odsnum = odsnum;
     this._odsdate = odsdate;
     this._ownerid = ownerid;
-    //
+    //liste oggetti
     this._equip=[];
     this._works=[];
     this._materials=[];
@@ -226,22 +226,3 @@ class Signature {
     return this._signature;
   }
 }
-
-var ticket= new Ticket('26-05-2022','alessandro cescon','123456','25-05-2022','001');
-var eqi = new Equipment('GEN_SET','999999');
-ticket.addequi(eqi);
-var wor = new Work();
-ticket.addwork(wor);
-var mat = new Material();
-ticket.addmaterial(mat);
-var not = new Note();
-ticket.addnote(not);
-var tsi = new Signature();
-ticket.addtecsign(tsi);
-var csi = new Signature();
-ticket.addclisign(csi);
-
-
-
-console.log(ticket);
-//ticket.adsnum = '9999999999';
