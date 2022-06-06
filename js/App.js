4463cee6fd1b47d9878318677c38bc105019a8c3
acescon <acescon@visa.it>
2022-06-06 14:35:21 +0200

    dev

--- js/App.js
+++ js/App.js
@@ -92,7 +92,7 @@ class App {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.withCredentials = true;
-    xhr.open('POST', "http://webauthtest.visa.visa/user/login?_format=json", true);
+    xhr.open('POST', "http://webauthtest.visa.visa/remotelogin", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function () {
       if (this.readyState == 4 && this.status == 200) {
