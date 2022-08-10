e002566900c9e32e803b08b73d012c608274c0ca
acescon <acescon@visa.it>
2022-08-10 15:46:37 +0200

    passaggio a jscms

---
+++ menu.js
@@ -0,0 +1,11 @@
+var pages = {
+  "pages": [
+    { "menu":"Home", "title":"home page", "folder":"", "url":"index.html" },
+    { "menu":"Per iniziare", "title":"considerazioni iniziali sul sito", "folder":"", "url":"contenuto.html" },
+    { "menu":"Art1", "title":"articolo numero 1", "folder":"", "url":"articolo-uno.html" },
+    { "menu":"Art2", "title":"articolo numero 2", "folder":"", "url":"articolo-due.html" },
+    { "menu":"Art3", "title":"articolo numero 3", "folder":"", "url":"articolo-tre.html" },
+    { "menu":"Art4", "title":"articolo numero 4", "folder":"", "url":"articolo-quattro.html" },
+    { "menu":"Geotest", "title":"Test Javascript Geolocation", "folder":"", "url":"tracker.html" }
+  ]
+}
