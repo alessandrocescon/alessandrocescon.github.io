65ed4edf7cd682c925897081d83df29d77c3a119
acescon <acescon@visa.it>
2022-10-17 15:13:09 +0200

    ww

--- menu.js
+++ menu.js
@@ -5,6 +5,7 @@ var pages = {
     { "menu":"Art1", "title":"articolo numero 1", "folder":"content", "url":"articolo-uno.html" },
     { "menu":"Art2", "title":"articolo numero 2", "folder":"content", "url":"articolo-due.html" },
     { "menu":"T-Shirt Ciao Mondo", "title":"T-Shirt Ciao Mondo", "folder":"content/t-shirt-ciao-mondo", "url":"t-shirt-ciao-mondo.html" },
-    { "menu":"Demo", "title":"Test Javascript Dyamic load dmeo", "folder":"content", "url":"demo.html" }
+    { "menu":"Demo", "title":"Test Javascript Dyamic load dmeo", "folder":"content", "url":"demo.html" },
+    { "menu":"Web Worker", "title":"web worker", "folder":"content", "url":"web-worker.html" }
   ]
 }
